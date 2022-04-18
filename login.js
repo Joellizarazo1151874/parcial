3711b0d79437c1e9dd1b48bea66134a87c44bb93
Joel lizarazo <80435721+Joellizarazo1151874@users.noreply.github.com>
2022-04-18 12:10:53 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,36 @@
+function loginFunc(e)
+{
+    //event.preventDefault();
+
+    var username = document.getElementById('codigo').value;
+    var clave = document.getElementById('password').value;
+
+
+    var urllogin = 'https://api-parcial.crangarita.repl.co/login';
+    var data = {user: username, password: clave};
+    
+    fetch(urllogin, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => {
+                        console.log('Success:', response);
+                        localStorage.setItem("user", response.user);
+                        localStorage.setItem("name", response.name);
+                        window.location.href="html/dashboard.html";
+                      }
+         );
+}
+
+if(localStorage.getItem("user")==null) {
+  window.alert("Datos incorrectos");
+}
+if(localStorage.getItem("user")!=null) {
+  window.location.href="html/dashboard.html";
+}
+
+
